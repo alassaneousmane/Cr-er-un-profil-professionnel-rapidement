e19507a08b25227371e246e20f1caa3d067707f4
alassaneousmane <alassaneousmane071@gmail.com>
2016-10-29 16:47:50 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Standard Meta -->
+  
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+      <!-- Site Properties -->
+      <title>Ousmane ALASSANE | Recherche contrat d'alternance</title>
+
+      <!-- CSS  -->
+      <link rel="icon" type="image/png" href="assets/pictures/Ousmane-ALASSANE.jpg" />
+      <link rel="stylesheet" href="font-awesome-4.3.0/css/font-awesome.min.css">
+
+ 
+          <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/reset.css">
+           <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/semantic.min.css">
+           <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/docs.css">
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/site.css">
+
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/container.css">
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/grid.css">
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/header.css">
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/image.css">
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/menu.css">
+
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/divider.css">
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/segment.css">
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/form.css">
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/input.css">
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/button.css">
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/list.css">
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/message.css">
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/icon.css">
+             <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/divider.css">
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/list.css">
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/segment.css">
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/dropdown.css">
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/icon.css">
+            <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/dist/components/transition.css">
+          
+          
+          <link rel="stylesheet" href="assets/css/main.css">
+  
+         
+        
+           <meta charset="utf-8"/>
+  
+
+</head>
+<body style="background-color: #e9eaed">
+
+  <div class="ui main text container">
+    <h1 class="ui header">Mon objectif professionnel</h1>
+    <p>Devenir expert en cybersécurité et en développement web!</p>
+  </div>
+
+
+  <div class="ui borderless main menu" style="background-color: #e9eaed">
+    <div class="ui text container">
+      <div href="#" class="header item">
+        <img class="logo" src="assets/pictures/Ousmane-ALASSANE.jpg">
+        Ousmane ALASSANE
+      </div>
+      <a href="#formations" class="item">Formations</a>
+      <a href="#experiences" class="item">Expériences professionnelles</a>
+      <a href="#competences" class="item">Compétences</a>
+  
+    </div>
+  </div>
+
+  <div class="ui text container">
+   <div class="ui raised segment">
+  <p id="formations" style="font-size: 19px; font-weight: bold">&nbsp;&nbsp;&nbsp;<i class="fa fa-briefcase" aria-hidden="true"></i>&nbsp;
+Formations</p>
+  <p><blockquote>2015 - 2016 : Préparation d'un Master 1 Réseaux et Télécoms par apprentissage à l'Université de Paris-Sud &nbsp;<br/>2015 - 2016 : Licence Professionnelle Réseaux et Télécoms à l'Université de Valenciennes et du Hainaut-Cambrésis<br/>2013 - 2015 : BTS en Réseaux et Systèmes à l'INSFP d'Ain-Témouchent (Major de promotion)<br/>
+  2011 - 2012 : Licence 1 Maths-Info à la Faculté des Sciences et Techniques de Bamako<br/>
+  2010 - 2011 : Baccalauréat -Serie Sciences Exactes</blockquote></p>
+</div>
+  <div class="ui raised segment">
+  <p id="experiences"style="font-size: 19px; font-weight: bold">Expériences professionnelles</p>
+  <p><div>Mars 2016 - Juillet 2016<br/><strong><blockquote>Développeur d'applications web</strong><br/>Eurl Ilouane<br/> 1. Réaliser une application de messagerie instantanée.<br/>
+ 2. Créer une application web CRM(Customer Relationship Manager).<br/>
+ 3. Créer une application d'édition de factures : calcul automatique des prix et
+sortie du fichier en format PDF.<br/>
+ 4. Résultats: Le logiciel d'édition de factures fut un succès et a permis de rendre
+cette tâche beaucoup plus facile. L'application de messagerie permet à deux
+collaborateurs d'envoyer des conversations instantanément. Le CRM n'est pas
+réaliser à 100 % mais les codes bien documentés vont permettre à un futur
+stagiaire de travailler sans difficulté.
+</div>
+</blockquote>
+
+<div>Octobre 2015 - Décembre 2015<br/><strong><blockquote>Etudiant-chercheur</strong><br/>Université de Valenciennes et du Hainaut-Cambrésis<br/> État de l'art sur les communications V2V(Véhicules connectés).<br/>
+Etudier les différentes technologies sans fil entrant en jeu.<br/>
+Evaluer l'aspect sécurité.<br/>
+Résultat : Ce projet fut fascinant. Je garde l'importante des communications sans
+fil pour le développement du transport routier. Les véhicules connectés rendront
+notre transport plus fluide et éviteront bon nombres d'accidents.
+</div>
+</blockquote>
+<div>Mars 2015 - Juin 2015<br/><strong><blockquote>Stagiaire Chef de projet e-learning</strong><br/>INSFP d'Ain-Témouchent - Algérie<br/> 1. Établir un cahier de charges pour un projet e-learning.<br/>
+2. Effectuer un choix entre différentes plateformes e-learning.<br/>
+3. Mettre en place une plateforme e-learning pour un centre de formation.<br/>
+4. Résultats : La plateforme réalisée avec Moodle permet à un professeur de
+déposer ses cours et devoirs. Les étudiants ont ainsi la possibilité d'apprendre
+quand et où ils veulent. L'enseignement à distance connait un réel succès de nos
+jours.
+</div>
+</blockquote></p>
+</div>
+    <div class="overlay">
+      <div class="ui labeled icon vertical menu">
+        <a class="item" href="https://www.linkedin.com/in/ousmane-alassane-57256a66?trk=nav_responsive_tab_profile" target="_blank"><i class="linkedin icon"></i> Linkedin</a>
+           <a href="mailto:alassaneousmane071@gmail.com" class="item"><i class="mail icon"></i> Envoyez-moi un mail</a>
+      </div>
+    </div>
+    <p>L'expérience la plus pertinente fut le stage que j'ai effectué au sein de l'entreprise Ilouane en développement web. J'ai su faire preuve de grande autonomie et de perspicacité. Je souhaite désormais développer mes compétences et connaissances avec une formation en alternance me permetttant de bénéficier d'une expérience sur le terrain avec des experts. </p>
+    <p>
+<div class="ui card" style="margin-left: 220px; height: 780px">
+  <div class="image">
+    <img src="assets/pictures/OusmaneALASSANE.jpg" height="15px">
+  </div>
+  <div class="content">
+    <a class="header">Ousmane ALASSANE</a>
+    <div class="meta">
+      <span class="date">Joined in 2016 University of Paris-Sud</span>
+    </div>
+    <div class="description" style="font-size: 13px">Je souhaite suivre une formation en Master 1 Réseaux et Télécoms en alternance à l'Université de Paris-Sud avec une entreprise qui acceptera de me former sur leurs secteurs d'activité. </div>
+  </div>
+  <div class="extra content">
+    <a><i class="user icon"></i> 23 ans(né le 13/08/1993) </a>
+  </div>
+</div>
+  <div class="ui raised segment">
+  <strong><p id="competences" style="font-size: 19px">Mes compétences</p></strong>
+    <table class="ui very basic collapsing celled table">
+  <thead>
+    <tr><th>Domaines</th>
+    <th>Niveau acquis</th>
+  </tr></thead>
+  <tbody>
+    <tr>
+      <td>
+        <h4 class="ui image header">
+          <img src="assets/pictures/reso.jpg" class="ui mini rounded image">
+          <div class="content">Réseaux <div class="sub header">Administration réseaux </div>
+        </div>
+      </h4></td>
+      <td>
+      <div class="ui star rating" data-rating="5" data-max-rating="5"></div>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <h4 class="ui image header">
+          <img src="assets/pictures/TELECOMS.jpg" class="ui mini rounded image">
+          <div class="content">Télécoms <div class="sub header">Planification radio, couverture et déploiement </div>
+        </div>
+      </h4></td>
+      <td>
+      <div class="ui star rating" data-rating="3" data-max-rating="5"></div>
+      </td>
+    </tr>
+      <tr>
+      <td>
+        <h4 class="ui image header">
+          <img src="assets/pictures/systèmes.png" class="ui mini rounded image">
+          <div class="content">Systèmes <div class="sub header">Windows 2008/2012 Server | Linux/Débian </div>
+        </div>
+      </h4></td>
+      <td>
+      <div class="ui star rating" data-rating="4" data-max-rating="5"></div>
+      </td>
+    </tr>
+      <tr>
+      <td>
+        <h4 class="ui image header">
+          <img src="assets/pictures/security.jpg" class="ui mini rounded image">
+          <div class="content">Sécurité <div class="sub header">Firewall, ACL, nmap, VPN, DMZ, Reverse Proxy ... </div>
+        </div>
+      </h4></td>
+      <td>
+      <div class="ui star rating" data-rating="4" data-max-rating="5"></div>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <h4 class="ui image header">
+          <img src="assets/pictures/planet.png" class="ui mini rounded image">
+          <div class="content">Développement web <div class="sub header">PHP5.6.0, Symfony3,... </div>
+        </div>
+      </h4></td>
+      <td>
+      <div class="ui star rating" data-rating="5" data-max-rating="5"></div>
+      </td>
+    </tr>
+   
+  </tbody>
+</table>
+</div></p>
+    
+  </div>
+
+  <div class="ui inverted vertical footer segment">
+    <div class="ui center aligned container">
+      <div class="ui stackable inverted divided grid">
+        </div>
+        <div class="seven wide column">
+          <h4 class="ui inverted header">À propos</h4>
+          <p>Ce site renferme les informations générales sur mon parcours et expériences professionnelles. "Je me suis dit que je peux mettre en évidence mes compétences en développement afin de réaliser cela. C'est peut-être plus facile pour un recruteur de se retrouver."</p>
+        </div>
+      </div>
+      <div class="ui inverted section divider"></div>
+      <img src="assets/pictures/OusmaneALASSANE.jpg" class="ui centered mini image">
+      <div class="ui horizontal center inverted small divided link list">
+        <div class="ui center aligned container" style="position:absolute; left: 80px">Copyright @2016 Ousmane ALASSANE</div>
+      </div>
+    </div>
+  </div>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="Semantic-UI-CSS-master/dist/components/transition.js"></script>
+  <script src="Semantic-UI-CSS-master/dist/semantic.min.js"></script>
+  <script src="Semantic-UI-CSS-master/dist/components/dropdown.js"></script>
+      <script src="Semantic-UI-CSS-master/dist/components/visibility.js"></script>    
+      <script src="Semantic-UI-CSS-master/dist/components/form.js"></script>
+      <script src="Semantic-UI-CSS-master/dist/components/transition.js"></script>
+      <script src="Semantic-UI-CSS-master/dist/components/tablesortmin.js"></script>
+      <script src="Semantic-UI-CSS-master/dist/components/clipboard.min.js"></script>
+       <script src="Semantic-UI-CSS-master/dist/components/cookie.min.js"></script>
+       <script src="Semantic-UI-CSS-master/dist/components/easing.min.js"></script>
+             
+            <script src="Semantic-UI-CSS-master/dist/components/history.min.js"></script>
+              <script src="Semantic-UI-CSS-master/dist/components/highlight.min.js"></script>
+              <script>
+  $(document)
+    .ready(function() {
+      $('.ui.rating')
+  .rating('disable', true)
+;
+
+      // fix main menu to page on passing
+      $('body').css('body', '#e9eaed');
+      $('.main.menu').visibility({
+        type: 'fixed'
+      });
+      $('.overlay').visibility({
+        type: 'fixed',
+        offset: 80
+      });
+
+      // lazy load images
+      $('.image').visibility({
+        type: 'image',
+        transition: 'vertical flip in',
+        duration: 500
+      });
+
+      // show dropdown on hover
+      $('.main.menu  .ui.dropdown').dropdown({
+        on: 'hover'
+      });
+    })
+  ;
+  </script>
+</body>
+
+</html>
